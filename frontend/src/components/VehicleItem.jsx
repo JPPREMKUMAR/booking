a27ec8797aef50862926d37e38dfe6d0ec6c007c
JPPREMKUMAR <jppremkumar012@gmail.com>
2025-09-24 15:58:39 +0530

    Update VehicleItem.jsx

--- frontend/src/components/VehicleItem.jsx
+++ frontend/src/components/VehicleItem.jsx
@@ -13,19 +13,19 @@ const VehicleItem = ({ item }) => {
 
     return (
 
-        <div className="bg-white pb-3 px-4 py-4 rounded-lg shadow-lg flex  flex-col items-center gap-3 m-5 transform transition-transform duration-300 ease-out hover:scale-120 ">
-            <img src={imageUrl} alt={`${vehicleName}`} className="w-2/3 h-30 sm:h-44" />
+        <div className="bg-white pb-3 px-4 py-4 rounded-lg shadow-lg flex  flex-col items-center gap-3 sm:m-5 m-2 mb-2  transform transition-transform duration-300 ease-out hover:scale-120 ">
+            <img src={imageUrl} alt={`${vehicleName}`} className="w-2/3 h-35 sm:h-44" />
             <h1 className="text-lg text-gray-800 font-bold text-medium sm:text-2xl">{vehicleName}</h1>
-            <div className="flex items-center gap-x-2 sm:text-xl">
+            <div className="flex items-center gap-x-2 text-md sm:text-xl">
                 <IoPeopleSharp className="text-gray-600" />
-                <p className="text-gray-600 text-sm  sm:text-xl ">Capacity : {capacity}+1</p>
+                <p className="text-gray-600 text-md sm:text-xl ">Capacity : {capacity}+1</p>
             </div>
             <div className="flex items-center gap-x-2">
-                <p className="text-gray-600 text-sm  sm:text-xl font-semibold">A/C </p>
-                <p className="text-gray-600 text-sm  sm:text-xl font-semibold">₹</p>
-                <p className="text-gray-600 text-sm  sm:text-xl font-semibold">{price}</p>
+                <p className="text-gray-600 text-md  sm:text-xl font-semibold">A/C </p>
+                <p className="text-gray-600 text-md  sm:text-xl font-semibold">₹</p>
+                <p className="text-gray-600 text-md  sm:text-xl font-semibold">{price}</p>
             </div>
-            <button type="button" className="bg-blue-400 px-5 py-1 rounded-md text-sm sm:text-xl sm:px-6 text-slate-200 hover:bg-blue-600 hover:text-white mb-5 mt-2">Book Now</button>
+            <button type="button" className="sm:bg-blue-400 bg-blue-600 text-white px-5 py-2 rounded-md text-md sm:text-xl sm:px-6 sm:text-slate-200 hover:bg-blue-600 hover:text-white mb-5 mt-2">Book Now</button>
         </div>
     )
 }
