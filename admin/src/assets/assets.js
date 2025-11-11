a380a84034df34ab220bc79a3fb7006fc363eca4
JETTI PRASANGI PREM KUMAR <jppremkumar012@gmail.com>
2025-11-11 03:11:09 -0800

    new Update

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,7 @@
 
-import menu_icon from "./menu_icon.png"
-import cross_icon from "./cross_icon.png"
-import logo from "./logo.svg"
+import menu_icon from "./menu.png"
+import cross_icon from "./close.png"
+import logo from "./web_logobg.png"
 import image1 from "./1.png"
 import image2 from "./2.png"
 import image3 from "./3.png"
@@ -10,6 +10,22 @@ import image5 from "./5.png"
 import image6 from "./6.png"
 
 
+import whatsapp from "./whatsapp.png"
+import telephone from "./telephone.png"
+
+import instagram from "./instagram.svg"
+import facebook from "./facebook.svg"
+import twitter from "./twitter-circle.svg"
+import linkedIn from "./linkedin-circle.svg"
+import gmail from './gmail.svg'
 
 
-export { menu_icon, cross_icon, logo, image1, image2, image3, image4, image5, image6 }
+
+export {
+    menu_icon, cross_icon,
+
+    instagram, facebook, twitter, linkedIn, gmail
+    ,
+
+    logo, image1, image2, image3, image4, image5, image6, whatsapp, telephone
+}
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom'
 import { MainContext } from "../context/MainContext"
 import { logo, menu_icon, cross_icon } from "../assets/assets"
 import Cookies from "js-cookie"
+
+import { IoMenu } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
+
+
+
+
+
 const Navbar = () => {
 
     const { token, navigate, setToken } = useContext(MainContext)
@@ -29,23 +37,28 @@ const Navbar = () => {
     return (
 
 
-        <div className="bg-gray-200 px-5 py-5 w-full">
-            <div className="flex justify-between  items-center sm:w-full sm:justify-between ">
+        <div className=" w-full  bg-[#0D1B2A]">
+            <div className="flex justify-between  items-center sm:w-full sm:justify-between px-5 py-2 ">
 
                 <div className="sm:w-1/3" onClick={() => navigate("/")} >
                     <img src={logo} alt="Logo" width={130} height={100} />
                 </div>
 
-                <button onClick={changeManu} className="sm:hidden flex items-center justify-center">
-                    {
-                        isMenu ? <img src={cross_icon} alt="Cross" width={30} height={30} /> : <img src={menu_icon} alt="Menu" width={40} height={30} />
-                    }
-                </button>
+                <div className='sm:hidden text-center flex items-center w-[30px] h-[30px] '>
+                    <button type="button" onClick={() => setIsMenu((prev) => !prev)}>
+                        {
+                            isMenu ? <RxCross1 className="w-[30px] h-[30px] text-white " />
+                                : <IoMenu className="w-[30px] h-[30px] text-white " />
+                        }
+
+                    </button>
+                </div>
 
                 <div className="hidden  sm:block sm:flex justify-around gap-x-15  items-center w-2/3">
-                    <Link to="/" className="text-md font-semibold " ><p>Home</p></Link>
-                    <Link to="/addItem" className="text-md font-semibold "><p>Add Vehicle</p></Link>
-                    <Link to="/allVehicles" className="text-md font-semibold "><p>All Vehicles</p></Link>
+
+                    <Link to="/" className="text-xl text-white font-semibold " ><p>Home</p></Link>
+                    <Link to="/addItem" className="text-xl text-white font-semibold "><p>Add Vehicle</p></Link>
+                    <Link to="/allVehicles" className="text-xl text-white font-semibold "><p>All Vehicles</p></Link>
                     {
                         token === undefined || token === "" ? <Link to="/login" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)}>
                             <button className="bg-blue-500 text-white rounded-md px-8 py-2 ">Login</button>
@@ -59,24 +72,29 @@ const Navbar = () => {
 
             </div>
 
-            {
-                isMenu && <div className="sm:hidden flex flex-col gap-y-2 my-5">
+            <div>
+                {/*/ Small Devices /*/}
 
-                    <Link to="/" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)}><p>Home</p></Link>
-                    <Link to="/addItem" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)} ><p>Add Vehicle</p></Link>
-                    <Link to="/allVehicles" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)}><p>All Vehicles</p></Link>
-                    {
-                        token === undefined || token === "" ? <Link to="/login" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)}>
+                {
+                    isMenu && <div className="sm:hidden flex flex-col gap-y-4  p-4 ">
 
-                            <button className="bg-blue-500 text-white rounded-md px-8 py-2 ">
-                                Login
-                            </button>
-                        </Link> :
-                            <button onClick={onClickLogout} className="text-white bg-red-500 rounded-md px-2 py-2 outline-none w-30 font-semibold cursor-pointer">Logout</button>
-                    }
 
-                </div>
-            }
+                        <Link to="/" className="text-md text-white font-semibold" onClick={(prev) => setIsMenu(!prev)}><p>Home</p></Link>
+                        <Link to="/addItem" className="text-md text-white font-semibold" onClick={(prev) => setIsMenu(!prev)} ><p>Add Vehicle</p></Link>
+                        <Link to="/allVehicles" className="text-md text-white font-semibold" onClick={(prev) => setIsMenu(!prev)}><p>All Vehicles</p></Link>
+                        {
+                            token === undefined || token === "" ? <Link to="/login" className="text-md font-semibold" onClick={(prev) => setIsMenu(!prev)}>
+
+                                <button className="bg-blue-500 text-white rounded-md px-8 py-2 ">
+                                    Login
+                                </button>
+                            </Link> :
+                                <button onClick={onClickLogout} className="text-white bg-red-500 rounded-md px-2 py-2 outline-none w-30 font-semibold cursor-pointer">Logout</button>
+                        }
+
+                    </div>
+                }
+            </div>
         </div>
     )
 }
