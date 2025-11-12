a8e036c1635f0d97fc369ae09a980ac3be993153
JETTI PRASANGI PREM KUMAR <jppremkumar012@gmail.com>
2025-11-12 05:58:51 -0800

    carousel

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+
+const images = [
+    "https://res.cloudinary.com/dokbp23jt/image/upload/v1762929119/cab-booking-images/iwsogvq3tope9mgxjbie.png",
+    "https://res.cloudinary.com/dokbp23jt/image/upload/v1762929119/cab-booking-images/iwsogvq3tope9mgxjbie.png",
+    "https://res.cloudinary.com/dokbp23jt/image/upload/v1762929119/cab-booking-images/iwsogvq3tope9mgxjbie.png",
+    "https://res.cloudinary.com/dokbp23jt/image/upload/v1762929119/cab-booking-images/iwsogvq3tope9mgxjbie.png",
+
+
+
+
+];
+
+const Carousel = () => {
+    const [index, setIndex] = useState(0);
+
+    // Auto slide every 4 seconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % images.length);
+        }, 4000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="relative w-full  overflow-hidden mb-5">
+            {/* Slides */}
+            <div
+                className="flex transition-transform duration-700 ease-in-out"
+                style={{ transform: `translateX(-${index * 100}%)` }}
+            >
+                {images.map((img, i) => (
+                    <img
+                        key={i}
+                        src={img}
+                        alt={`Slide ${i}`}
+                        className="w-full h-64 md:h-96 flex-shrink-0 object-cover"
+                    />
+                ))}
+            </div>
+
+            {/* Navigation Dots */}
+            <div className="absolute bottom-3 left-0 right-0 flex justify-center gap-2">
+                {images.map((_, i) => (
+                    <button
+                        key={i}
+                        onClick={() => setIndex(i)}
+                        className={`w-3 h-3 rounded-full transition-all ${i === index ? "bg-white scale-125" : "bg-gray-400"
+                            }`}
+                    ></button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,15 @@ import VehicleList from "../components/VehicleList"
 import WhyBookWithUs from "../components/WhyBookWithUs"
 import ContactWithUs from "../components/ContactWithUs"
 import BookingCard from "../components/BookingCard"
-import ContactDetails from "../components/ContactDetails.jsx"
+import ContactDetails from "../components/ContactDetails"
+import Carousel from "../components/Carousel"
 
 const Home = () => {
 
     return (
         <div className="px-2 my-5">
 
+            <Carousel />
 
             <BookingCard />
 
