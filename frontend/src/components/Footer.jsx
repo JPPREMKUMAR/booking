3901678470f6380914b6ddb86439ba1403a37797
JETTI PRASANGI PREM KUMAR <jppremkumar012@gmail.com>
2025-12-18 01:18:36 -0800

    mm

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,11 +12,11 @@ const Footer = () => {
 
     return (
 
-        <div className="w-full mx-5">
-            <div className="w-[90%]  fixed bottom-10  flex items-center  ">
+        <div className="w-full">
+            <div className="w-full mx-2 fixed bottom-10  flex items-center  ">
 
 
-                <div className="sm:h-25 w-[15%]">
+                <div className="sm:h-25 w-[15%] sm:flex justify-center items-center ">
                     <a href={`https://wa.me/91${adminPhoneNumber1}`} target="_blank"
                         className="mx-5" aria-label="Chat on Whatsapp"
                     >
@@ -24,7 +24,7 @@ const Footer = () => {
                     </a>
                 </div>
                 <div className="sm:h-25 w-[70%]"></div>
-                <div className="sm:h-25 w-[15%]">
+                <div className="sm:h-25 w-[15%] sm:flex justify-center items-center">
 
                     <a href={`tel:+91${adminPhoneNumber1}`} className="mx-5" aria-label="Call Us">
                         <img src={telephone} alt="telephone" className="w-10 h-10  sm:hover:w-20 sm:hover:h-20" />
